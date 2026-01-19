3e0f6042679033bfcdf06dafa7606ff371520d11
rutulWeb <rutul.panchal.web@gmail.com>
2026-01-19 18:11:55 +0530

    Remove LiveKit configuration comments
    Removed commented LiveKit configuration variables.

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,3 @@ import App from "./App.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
-
-// LIVEKIT_URL=wss://screenshare-b1ok5kmf.livekit.cloud
-// LIVEKIT_API_KEY=APIKJME2ZeAsXAM
-// LIVEKIT_API_SECRET=CfVHsYS6I923kC8kk45L1NeB9uefPFobOeA3iJbVh8YA
